743427d7931423d40968c7c18a6fbae08c63a788
Jonas <jonas.goddon@t-online.de>
2025-12-02 21:48:42 +0100

    add simple day_01

---
+++ src/bin/day_01.rs
@@ -0,0 +1,60 @@
+use std::{cmp::max, fs};
+
+fn get_input() -> String {
+    let path = std::path::Path::new("./inputs/01.txt");
+    let content: String = fs::read_to_string(path).expect("Failed to read File.");
+    return content;
+}
+
+fn main() {
+    let input = get_input();
+
+    let mut pos = 50;
+    let mut res_1 = 0;
+
+    for line in input.split('\n') {
+        let (first, rest) = line.split_at(1);
+        let mut offset: i32 = rest.parse().unwrap();
+
+        offset = match first {
+            "L" => -offset,
+            "R" => offset,
+            _ => panic!("unknown char"),
+        };
+
+        pos += offset;
+
+        pos = ((pos % 100) + 100) % 100;
+
+        if pos == 0 {
+            res_1 += 1;
+        }
+    }
+
+    println!("{}", res_1);
+
+    let mut pos = 50;
+    let mut res_2 = 0;
+
+    for line in input.split('\n') {
+        let (first, rest) = line.split_at(1);
+        let mut offset: i32 = rest.parse().unwrap();
+        println!("{} - {}", first, rest);
+
+        offset = match first {
+            "L" => -offset,
+            "R" => offset,
+            _ => panic!("unknown char"),
+        };
+
+        pos += offset;
+
+        if pos >= 100 || pos <= 0 {
+            res_2 += max((offset / 100).abs(), 1);
+            println!("Passes zero {} {}", res_2, pos);
+        }
+
+        pos = ((pos % 100) + 100) % 100;
+    }
+    println!("part2: {}", res_2);
+}
